6161108f0f28b38ebe30f44d70d9a20e234161c2
Yohnah <eejdelgado@ws03500.global.cciberianp.net>
2015-08-12 19:38:10 +0100

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name="pyadldap",
-	version="0.1.0",
+	version="0.1.1",
 	description="Library for managing Microsoft Active Directory from LDAP",
 	author="Jonas Delgado Mesa",
 	author_email="jdelgado@yohnah.net",
