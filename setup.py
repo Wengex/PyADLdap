6a74fc8b4815428769a74cd646c58cdec5ddf634
Yohnah <eejdelgado@ws03500.global.cciberianp.net>
2015-08-13 12:52:04 +0100

    dependencies added in the setup.py file

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 
 setup(
 	name="pyadldap",
-	version="0.1.1",
-	description="Library for managing Microsoft Active Directory from LDAP",
+	version="0.1.2",
+	description="FrameWork for managing Microsoft Active Directory from LDAP",
 	author="Jonas Delgado Mesa",
 	author_email="jdelgado@yohnah.net",
 	url="https://github.com/Wengex/PyADLdap",
 	license="GPLv2",
 	packages=["pyadldap"],
 	long_description=open('README.md').read(),
+	install_requires = ['python-ldap']
 )
